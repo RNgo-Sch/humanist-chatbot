15671cea3beadb9c925811c00c4f654fe0f651b6
jovyan <jovyanqiu@gmail.com>
2026-03-12 23:01:36 +0800

    Added Streamlit UI

--- tts.py
+++ tts.py
@@ -1,8 +1,8 @@
 import pyttsx3
 
-engine = pyttsx3.init()
-
 def speak(text):
     print("Assistant:", text)
+    engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+    engine.stop()
